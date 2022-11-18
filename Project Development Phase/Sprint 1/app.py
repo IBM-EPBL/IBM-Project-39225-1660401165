de9be4dc8fb2c960326afcc66907fa8bc6122c12
KISHORE LAXMAN <kishorelaxman@KISHOREs-MacBook-Air.local>
2022-11-19 01:34:49 +0530

    Project Development Phase added

---
+++ Project Development Phase/Sprint 1/app.py
@@ -0,0 +1,27 @@
+from collections import UserDict
+from flask import Flask,render_template,request,redirect,url_for,session
+import ibm_db
+import re
+
+msg = ""
+app =Flask(__name__)
+app.secret_key='a'
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login',methods =['GET','POST'])
+def login():
+    return render_template('login.html',msg=msg)
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    return render_template('register.html',msg=msg)
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port = "5000")    
+    
+
+
+
